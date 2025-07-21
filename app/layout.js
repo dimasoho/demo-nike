88eb35afb15b7735039c3ce80559ef2501a235e9
Dima Soho <info@dimasoho.com>
2025-07-21 16:32:25 +0200

    Tailwind setup

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,50 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// Importing custom fonts via NextJs instead of Tailwind for better performance
+const machina = localFont({
+  src: [
+    {
+      path: '../public/font/PPNeueMachina-InktrapLight.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/PPNeueMachina-InktrapRegular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/PPNeueMachina-InktrapMedium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-machina',
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const saol = localFont({
+  src: [
+    {
+      path: '../public/font/SaolStandard-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-saol',
+  display: 'swap',
 });
 
 export const metadata = {
-  title: "Nike - Air Max",
-  description: "DEMO",
+  title: "Nike - Air Max"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${machina.variable} ${saol.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.js
+++ app/page.js
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <section className="layout-grid py-32">
+      <p className="">
+        Hello World!
 
-    <h1>Nike - Air Max</h1>
+      </p>
+    </section>
   );
+
+
 }
